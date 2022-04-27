7bfa26745dd3a81845f0130b04d1c17c7e710a64
PRCV1 <lukas.mueller.01@web.de>
2022-04-27 23:19:33 +0200

    added impressum and datenschutz pages

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="py-5" :class="[isDarkmode ? 'bg-dark' : 'bg-light']">
+        <div class="container d-flex justify-content-center">
+            <BsLink url="/impressum" text="Impressum" />
+            <BsLink url="/datenschutz" text="Datenschutz" />
+            <BsLink url="test" text="Sitemap" />
+            <router-link to="/impressum">Go to Home</router-link>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { useDarkmode } from "../helper/useDarkmode"
+import BsLink from "./bsLink.vue";
+
+const { isDarkmode, setIsDarkmode } = useDarkmode()
+</script>
