4777cdc910d4e2dfb85c952f039f4de41d3ddd08
PRCV1 <lukas.mueller.01@web.de>
2022-04-27 23:21:16 +0200

    added impressum and datenschutz pages

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="py-5" :class="[isDarkmode ? 'bg-dark' : 'bg-light']">
+        <div class="container d-flex justify-content-center">
+            <router-link to="/impressum">Impressum</router-link>
+            <router-link to="/datenschutz">Datenschutzerklärung</router-link>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { useDarkmode } from "../helper/useDarkmode"
+
+const { isDarkmode, setIsDarkmode } = useDarkmode()
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-8 col-12">
+                <Section title="Über mich">
+                    Ich bin Softwareentwickler bei AFS-Software GmbH & Co.KG, einer Softwarefirma welche sich auf die Entwicklung und Vertrieb einer eigenen Warenwirtschaft und Kasse spezialisiert hat.
+                    Ich habe über 5 Jahre Erfahrung in der Backend-Entwicklung und arbeite mit verschiedenen Kunden in ganz Deutschland zusammen. Ich bin auch ein Fan von moderner App-Entwicklung mit Flutter.
+                </Section>
+                <Section title="Meine Projekte">
+                    <Projects />
+                </Section>
+            </div>
+            <div class="col-lg-4 col-12">
+                <Section title="Kontakt">
+                    <ol class="list-unstyled">
+                        <li class="mb-3">
+                            <i class="bi bi-geo-alt-fill"></i>
+                            Philippsthal (Werra), DE
+                        </li>
+                        <li class="mb-3">
+                            <i class="bi bi-geo-alt-fill"></i>
+                            Philippsthal (Werra), DE
+                        </li>
+                        <li>
+                            <i class="bi bi-link-45deg"></i>
+                            <BsLink
+                                class="text-decoration-none"
+                                :show-icon="false"
+                                text="lukasmueller.info"
+                                url="https://lukasmueller.info"
+                            />
+                        </li>
+                    </ol>
+                </Section>
+                <Section title="Fähigkeiten">
+                    <BsProgressbar label="VB.NET/C#" value="50" />
+                    <BsProgressbar label="SQL (MySQL/MSSQL)" value="60" />
+                    <BsProgressbar label="JavaScript" value="50" />
+                    <BsProgressbar label="EF Core" value="50" />
+                </Section>
+                <Section title="Ausbildung">
+                    <Education />
+                </Section>
+                <Section title="Credits"></Section>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Education from "../components/Education.vue";
+import Projects from "../components/Projects.vue";
+import BsProgressbar from "../components/bsProgressbar.vue";
+import BsLink from "../components/bsLink.vue";
+import Section from "../components/Section.vue";
+</script>
