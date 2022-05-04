ce02a20579a32a1137050c140a19dce3ba56d8a2
PRCV1 <lukas.mueller.01@web.de>
2022-05-04 23:07:07 +0200

    Header responsive adjustments

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,57 @@
 <template>
-    <header class="py-5" :class="[isDarkmode ? 'bg-dark' : 'bg-light']">
+    <header class="py-md-5 py-3" :class="[isDarkmode ? 'bg-dark' : 'bg-light']">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div
-                        class="d-flex align-items-center"
-                        :class="[isDarkmode ? 'text-white' : 'text-dark']"
-                    >
-                        <div class="flex-shrink-0">
-                            <img
-                                class="img-fluid rounded-circle"
-                                src="https://lukasmueller.info/_nuxt/img/77839d1.jpg"
-                                width="175"
-                            />
+                    <div class="row" :class="[isDarkmode ? 'text-white' : 'text-dark']">
+                        <div class="col-md-2 d-flex justify-content-center">
+                            <img class="img-fluid rounded-circle" src="https://lukasmueller.info/_nuxt/img/77839d1.jpg"
+                                width="175" />
                         </div>
-                        <div class="flex-grow-1 ms-3">
-                            <h1>Lukas Müller</h1>
-                            <h2>Anwendungsentwickler</h2>
+                        <div class="col-md-10 text-md-start text-center">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <h1>
+                                        Lukas Müller
+                                    </h1>
+                                </li>
+                                <li>
+                                    <h2>Anwendungsentwickler</h2>
+                                </li>
+                            </ul>
                             <ul class="list-inline">
-                                <li
-                                    v-for="(item, index) in socialLinks"
-                                    :key="index"
-                                    class="list-inline-item"
-                                >
-                                    <a
-                                        :href="String(item.url)"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
+                                <li v-for="(item, index) in socialLinks" :key="index" class="list-inline-item">
+                                    <a :href="String(item.url)" target="_blank" rel="noopener noreferrer">
                                         <i style="font-size: 2rem;" :class="item.icon"></i>
                                     </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
+                    <!-- <div class="d-flex align-items-center" :class="[isDarkmode ? 'text-white' : 'text-dark']">
+                        <div class="flex-shrink-0">
+                            <img class="img-fluid rounded-circle" src="https://lukasmueller.info/_nuxt/img/77839d1.jpg"
+                                width="175" />
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                            <h1>Lukas Müller</h1>
+                            <h2>Anwendungsentwickler</h2>
+                            <ul class="list-inline">
+                                <li v-for="(item, index) in socialLinks" :key="index" class="list-inline-item">
+                                    <a :href="String(item.url)" target="_blank" rel="noopener noreferrer">
+                                        <i style="font-size: 2rem;" :class="item.icon"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div> -->
                 </div>
-                <div class="col-12 col-md-auto">
+                <div class="col-12 col-md-auto d-flex justify-content-center">
                     <div class="form-check form-switch" @click="setIsDarkmode(!isDarkmode)">
-                        <input
-                            class="form-check-input"
-                            type="checkbox"
-                            id="flexSwitchCheckChecked"
-                            v-model="isDarkmode"
-                        />
-                        <label
-                            :class="[isDarkmode ? 'text-white' : 'text-dark']"
-                            class="form-check-label"
-                            for="flexSwitchCheckChecked"
-                        > Darkmode </label>
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked"
+                            v-model="isDarkmode" />
+                        <label :class="[isDarkmode ? 'text-white' : 'text-dark']" class="form-check-label"
+                            for="flexSwitchCheckChecked"> Darkmode </label>
                     </div>
                 </div>
             </div>
